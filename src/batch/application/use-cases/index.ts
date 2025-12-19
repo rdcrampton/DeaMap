c7bcde2423cc68e119e3ffbefba37f8c12458ec5
Victor <vgpastor08@gmail.com>
2025-12-19 14:43:25 +0100

    Implement admin API for job management: force reset, health check, and recovery of stuck jobs

--- src/batch/application/use-cases/index.ts
+++ src/batch/application/use-cases/index.ts
@@ -2,8 +2,14 @@
  * Batch Job Use Cases
  */
 
+// Core job management
 export * from "./CreateBatchJobUseCase";
 export * from "./ContinueBatchJobUseCase";
 export * from "./GetBatchJobStatusUseCase";
 export * from "./CancelBatchJobUseCase";
 export * from "./ListBatchJobsUseCase";
+
+// Job recovery and health
+export * from "./RecoverStuckJobsUseCase";
+export * from "./ForceResetJobUseCase";
+export * from "./GetJobsHealthUseCase";
